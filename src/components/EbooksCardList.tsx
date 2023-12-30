0f6b8cfed17abf4431a0cac6f79446700d0649d6
naviduNavoda <148700348+NavinduNavoda@users.noreply.github.com>
2023-12-30 23:30:32 +0530

    fix

--- src/components/EbooksCardList.tsx
+++ src/components/EbooksCardList.tsx
@@ -55,7 +55,7 @@ const Services = (props: any) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-24 justify-between">
       {services.map((e, i) => {
         return (
-          <div>
+          <div key = {i}>
             <ServiceCard data={e} type="service" />
           </div>
         );
