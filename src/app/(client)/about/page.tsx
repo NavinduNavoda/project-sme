6e5924b42dbd3908cb332b4417ad23ba3759e266
Tharindu Sandaruwan <127480493+Tharindu-s@users.noreply.github.com>
2023-12-31 19:36:10 +0530

    testing ebooks fetch

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -4,19 +4,19 @@ const About = () => {
   return (
     <div className="">
       <div className="min-h-screen bg-lowwhite flex flex-col justify-center items-center px-4 sm:px-4 md:px-24 lg:px-40 mb-24 md:mb-48">
-        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[50px] xl:mx-auto font-montserrat text-accentsme text-center mx-auto pb-8 mb-0 w-auto uppercase">
+        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[30px] xl:text-[40px] 2xl:text-[50px] xl:mx-auto font-montserrat text-accentsme text-center mx-auto pb-8 mb-0 w-auto uppercase">
           Our misson
         </h1>
-        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[30px] xl:mx-auto font-montserrat text-paragrapgh text-center mx-auto  mb-0 w-auto">
+        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[30px] 2xl:text-[30px] xl:mx-auto font-montserrat text-paragrapgh text-center mx-auto  mb-0 w-auto">
           Our mission is to provide managers and leaders in the SME sector with
           the desired tools, frameworks, and expertise to help them create and
           execute their strategic plans successfully.
         </h1>
 
-        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[50px] xl:mx-auto font-montserrat text-accentsme text-center mx-auto pt-16 pb-8 mb-0 w-auto uppercase">
+        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[30px] xl:text-[40px] 2xl:text-[50px] xl:mx-auto font-montserrat text-accentsme text-center mx-auto pt-16 pb-8 mb-0 w-auto uppercase">
           Our vision
         </h1>
-        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:text-[30px]  xl:mx-auto font-montserrat text-paragrapgh text-center mx-auto  mb-0 w-auto">
+        <h1 className="font-bold text-[20px] sm:text-[20px] md:text-[20px] lg:text-[20px] xl:text-[30px] 2xl:text-[30px]  xl:mx-auto font-montserrat text-paragrapgh text-center mx-auto  mb-0 w-auto">
           SME Strategic development company to be the pioneer in creating a mass
           network of lucrative and successful SME entrepreneurs in SRI LANKA to 
           the sustainable development of the Sri Lankan economy.
